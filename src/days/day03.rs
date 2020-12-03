052e86ac95d5121f39999f9e22968d9dcc23cd45
tgaede <3713202+tgaede@users.noreply.github.com>
2020-12-02 21:51:53 -0800

    day 3 initial commit

---
+++ src/days/day03.rs
@@ -0,0 +1,38 @@
+use crate::common::Solution;
+
+pub fn solve(lines: &[String]) -> Solution {
+    let part1: String = solve_part1(lines);
+    let part2: String = solve_part2(lines);
+
+    (part1, part2)
+}
+
+fn solve_part1(lines: &[String]) -> String {
+    count_trees(lines, 3, 1)
+}
+
+fn count_trees(lines: &[String], right: usize, down: usize) -> String {
+    let mut right_index: usize = 0;
+    let mut down_index: usize = 0;
+
+    lines
+        .iter()
+        .filter(|line| {
+            let result = (**line).chars().cycle().nth(right_index).unwrap() == '#'
+                && (down_index % down == 0);
+            right_index += right;
+            down_index += 1;
+            result
+        })
+        .count()
+        .to_string()
+}
+
+fn solve_part2(lines: &[String]) -> String {
+    (count_trees(lines, 1, 1).parse::<usize>().unwrap()
+        * count_trees(lines, 3, 1).parse::<usize>().unwrap()
+        * count_trees(lines, 5, 1).parse::<usize>().unwrap()
+        * count_trees(lines, 7, 1).parse::<usize>().unwrap()
+        * count_trees(lines, 1, 2).parse::<usize>().unwrap())
+    .to_string()
+}
